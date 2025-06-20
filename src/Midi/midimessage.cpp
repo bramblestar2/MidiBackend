85fecc8f31bce4d10dc49e7b52b1f4ed0f3c3a22
Jay Feather2000 <thegals4321@gmail.com>
2025-06-20 01:30:12 -0500

    more types

--- src/Midi/midimessage.cpp
+++ src/Midi/midimessage.cpp
@@ -1,4 +1,5 @@
 #include "midimessage.h"
+#include <array>
 
 int8_t MidiMessage::channel() noexcept {
     // Channel statuses start on 0x80 (128) and end on 0xEF (239)
@@ -15,11 +16,33 @@ int8_t MidiMessage::channel() noexcept {
 
 
 MidiMessage::Type MidiMessage::type() noexcept {
+    // constexpr std::array<Type, 16> types = {
+    //     Type::NoteOff,
+    //     Type::NoteOn,
+    //     Type::PolyphonicKeyPressure,
+    //     Type::ControlChange,
+    //     Type::ProgramChange,
+    //     Type::ChannelAftertouch,
+    //     Type::PitchBend
+    // };
+
+    // return types[(this->status & 0xF0) >> 4];
+
     switch (this->status & 0xF0) {
         case Type::NoteOff:
             return Type::NoteOff;
         case Type::NoteOn:
             return Type::NoteOn;
+        case Type::PolyphonicKeyPressure:
+            return Type::PolyphonicKeyPressure;
+        case Type::ControlChange:
+            return Type::ControlChange;
+        case Type::ProgramChange:
+            return Type::ProgramChange;
+        case Type::ChannelAftertouch:
+            return Type::ChannelAftertouch;
+        case Type::PitchBend:
+            return Type::PitchBend;
         default:
             return Type::UNKNOWN;
     }
--- src/Midi/midimessage.h
+++ src/Midi/midimessage.h
@@ -12,6 +12,11 @@ struct MidiMessage {
         UNKNOWN = 0x00,
         NoteOff = 0x80,
         NoteOn = 0x90,
+        PolyphonicKeyPressure = 0xA0,
+        ControlChange = 0xB0,
+        ProgramChange = 0xC0,
+        ChannelAftertouch = 0xD0,
+        PitchBend = 0xE0
     };
 
     int8_t channel() noexcept;
